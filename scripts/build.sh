e267e39ca773d2a8763e2f40e54d9248ceb65731
Phil Eaton <phil@eatonphil.com>
2022-10-28 15:57:40 +0000

    Add support for branch in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
+# Build a different branch by passing the branch name in as the first argument
+branch="main"
+if [[ -n "$1" ]]; then
+    branch="$1"
+fi
+echo "Building branch: $branch"
+
 set -eu
 
 # Grab the latest docs from tigerbeetledb/tigerbeetle
 git clone https://github.com/tigerbeetledb/tigerbeetle tb_tmp
 
-# If you need to check out a different branch, do so here
-# ( cd tb_tmp && git fetch && git checkout pe/clients-references )
+if ! [[ "$branch" == "main" ]]; then
+    ( cd tb_tmp && git fetch && git checkout "$branch" )
+fi
 
 rm -rf pages
 mv tb_tmp/docs pages
-find pages -maxdepth 1 -mindepth 1 -type f -not -name "intro.md" -delete
+for page in $(ls pages/*.md); do
+    if ! [[ "$page" == "pages/intro.md" ]] && ! [[ "$page" == "pages/FAQ.md" ]]; then
+	rm "$page"
+    fi
+done
 rm -rf tb_tmp
 
 # Build the site

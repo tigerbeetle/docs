b671e283752c46123f03ac320b4468ba02078412
Phil Eaton <phil@eatonphil.com>
2023-03-24 17:53:24 +0000

    Update diff

--- scripts/fail_on_diff.sh
+++ scripts/fail_on_diff.sh
@@ -3,7 +3,7 @@
 set -eu
 
 if [[ "$(git diff)" != "" ]]; then
-    printf "\033[0;31mFAILURE: Unexpected diff (did you run 'yarn format'?)\n\n\033[0m"
-    git diff --color=never
+    printf "\033[0;31mFAILURE: Unexpected diff (did you run './scripts/build.sh'?)\n\n\033[0m"
+    git diff -B --color=never
     exit 1
 fi

86acecc577ddfc5c7ad25136cd6be940d0b54512
Phil Eaton <phil@eatonphil.com>
2023-03-24 18:14:48 +0000

    Try upgrading things

--- scripts/fail_on_diff.sh
+++ scripts/fail_on_diff.sh
@@ -4,6 +4,6 @@ set -eu
 
 if [[ "$(git diff)" != "" ]]; then
     printf "\033[0;31mFAILURE: Unexpected diff (did you run './scripts/build.sh'?)\n\n\033[0m"
-    git diff -B --color=never
+    git diff --color=never
     exit 1
 fi

98865c1e0bc2dbdecbd7aed274b66dcb0a6891e2
Phil Eaton <phil@eatonphil.com>
2023-06-02 14:06:51 +0000

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@
 # Furthermore, for some reason in Github Actions (only), it complains
 # about "fatal: detected dubious ownership in repository" without this
 # add safe directory command.
-docker run -v $(pwd):/build -w /build node:19 bash -c "git config --global --add safe.directory /build && npm install && ./scripts/build_dont_call_this_directly.sh $@"
+docker run -v $(pwd):/build -w /build node:19 bash -c "git config --global --add safe.directory /build && npm install && ./scripts/build_dont_call_this_directly.sh $*"
